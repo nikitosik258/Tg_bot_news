23a2fd901ab256400e29d33a9e0c0d173012b4be
nikitosik258 <85572724+nikitosik258@users.noreply.github.com>
2023-07-17 12:59:23 +0200

    Парсер новостей и добавление сущностей

---
+++ parser_entinity/entity.py
@@ -0,0 +1,48 @@
+import sqlite3
+from natasha import (
+    MorphVocab,
+    NewsEmbedding,
+    Doc,
+    Segmenter,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+    PER,
+    LOC,
+    ORG,
+    NamesExtractor
+)
+
+# Создание экземпляров Natasha
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+ner_tagger = NewsNERTagger(emb)
+names_extractor = NamesExtractor(morph_vocab)
+
+
+def names_entity(texts):
+    # Обработка каждого текста
+    for text in texts:
+        text = text[0]
+        doc = Doc(text)
+
+        doc.segment(segmenter)
+        doc.tag_morph(morph_tagger)
+        doc.parse_syntax(syntax_parser)
+        doc.tag_ner(ner_tagger)
+
+        # Извлечение и добавление сущностей в таблицу entity
+        entities = []
+        for span in doc.spans:
+            if span.type in [PER, ORG]:
+                span.normalize(morph_vocab)
+                entities.append(span.normal)
+        name_entities = ', '.join(entities)
+
+        # Обновление столбца name_entity
+        with sqlite3.connect("news.db") as con:
+            cursor = con.cursor()
+            cursor.execute("UPDATE news SET name_entity = ? WHERE content = ?", (name_entities, text))
