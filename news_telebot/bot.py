104fae6df044d00c855bb43f8a478dcc33eb2624
Tatiana Ostrovskaya <85173137+ostaaaaat@users.noreply.github.com>
2023-07-21 20:43:35 +0300

    Add files via upload

---
+++ news_telebot/bot.py
@@ -0,0 +1,87 @@
+import asyncio
+import sys
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from telegram import Bot
+from telegram.constants import ParseMode
+from peewee import *
+
+db = SqliteDatabase('news.db')
+db.connect()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+        table_name = "news"
+
+
+class News(BaseModel):
+    id = PrimaryKeyField()
+    title = TextField()
+    url = TextField()
+    content = TextField()
+    annotation = TextField()
+    name_entity = TextField()
+    flag_annotation = IntegerField()
+    flag_news = IntegerField()
+
+
+bot = None
+updateIntervalSeconds = 10
+
+if len(sys.argv) > 2:
+    TOKEN = sys.argv[1]
+    CHAT_ID = sys.argv[2]
+    bot = Bot(TOKEN)
+    if len(sys.argv) > 3:
+        updateIntervalSeconds = sys.argv[3]
+else:
+    print("Usage: bot.py token channel_id [update_interval_seconds: default = 10]")
+    sys.exit()
+
+
+async def run():
+    news = News.select().where(
+        (News.flag_news.is_null() | News.flag_news == 0)
+        & (News.flag_annotation == 1)
+    )
+    for n in news:
+        tags = ' '.join(list(map(lambda tag: f'#{tag.replace(" ", "_")}', n.name_entity.split(', '))))
+        charsToEscape = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']
+        replace = ['\\' + l for l in charsToEscape]
+        trans = str.maketrans(dict(zip(charsToEscape, replace)))
+        message = f"""
+*{n.title.translate(trans)}*
+{n.url.translate(trans)}
+
+_{n.annotation.translate(trans)}_
+
+{tags.translate(trans)}
+"""
+        await bot.sendMessage(CHAT_ID,
+                              message,
+                              parse_mode=ParseMode.MARKDOWN_V2,
+                              disable_web_page_preview=True)
+        n.flag_news = 1
+        n.save()
+
+
+def _schedule():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(run, trigger=IntervalTrigger(seconds=updateIntervalSeconds))
+    scheduler.start()
+    print('Press Ctrl+C to exit')
+
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+
+if __name__ == "__main__":
+    _schedule()
+
+
+
